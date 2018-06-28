3267188b769b927f3ec0ba581804777f122eec9a
Brian Wandell <wandell@stanford.edu>
2018-06-27 17:27:00 -0700

    File types and tile size arguments

--- matlab/CreateSubImages.m
+++ matlab/CreateSubImages.m
@@ -1,5 +1,9 @@
-function CreateSubImages(originalTilesDir,subImageDir,varargin)
+function tileImages = CreateSubImages(originalTilesDir,subImageDir,varargin)
 % Start with original tiles, resize and save to subimage directory
+%
+% Description
+%  The images in originalTilesDir are converted to a tile size
+%  (default 32x32) and written out to the subImageDir
 %
 % Inputs
 %  originalTilesDir
@@ -7,6 +11,10 @@ function CreateSubImages(originalTilesDir,subImageDir,varargin)
 %
 % Key/value options
 %  image type  - 'jpg' is default but 'tif' or 'png' are possible
+%  tileSize    - Tile size
+%
+% Return
+%   tileImage - tileSize by nFiles tensor of images
 %
 % BW, 1995
 %
@@ -20,42 +28,58 @@ p = inputParser;
 p.addRequired('originalTilesDir',@(x)(exist(x,'dir')));
 p.addRequired('subImageDir',@(x)(exist(x,'dir')));
 p.addParameter('imagetype','jpg',@ischar);
+p.addParameter('tilesize',[32 32],@isvector);
 
 p.parse(originalTilesDir,subImageDir, varargin{:});
 
 imagetype = p.Results.imagetype;
+tileSize  = p.Results.tilesize;
 
 %% List the files
 
 cd(originalTilesDir);
 srch = ['*.',imagetype];
 imgFiles = dir(srch);
+nFiles = numel(imgFiles);
 
-for ii=1:numel(imgFiles)
-  fname = imgFiles(ii).name;
-  disp(fname);
-  % The r,g,b values run from [0,1].  So, we average them and
-  % then scale them up to nGray range for writing out as tiff.
-  % 
-  [im,mp] = imread(fname);
-  if ~isempty(mp), rgb = ind2rgb(im,mp); 
-  else, rgb = im;
-  end
-  % vcNewGraphWin; imagesc(rgb); % colormap(mp)
-  
-  rgb = imresize(rgb,[64 64]);
+tileImages = zeros(tileSize(1),tileSize(2),nFiles);
 
-  % Convert color images to black and white
-  im = double(rgb2gray(rgb));
-  im = ieScale(im,0,1);
-  % vcNewGraphWin; imshow(im);
-
-  % Write out the small image time
-  cd(subImageDir)
-  imwrite(im,fname); 
-  
-  cd(originalTilesDir);
-end  
+for ii=1:nFiles
+    fname = imgFiles(ii).name;
+    disp(fname);
+    % The r,g,b values run from [0,1].  So, we average them and
+    % then scale them up to nGray range for writing out as tiff.
+    %
+    [im,mp] = imread(fname);
+    % imshow(im);
+    
+    % Convert the image if index map.
+    if ~isempty(mp), rgb = ind2rgb(im,mp);
+    else, rgb = im;
+    end
+    
+    % Make it double and resize it
+    rgb = double(rgb);
+    tile = imresize(rgb,tileSize);
+    % vcNewGraphWin; imagesc(rgb); colormap(gray); axis image
+    
+    % Convert rgb images to black and white
+    if ndims(tile) == 3
+        tile = rgb2gray(tile);
+    end
+    
+    % Normalize
+    tile = ieScale(tile,0,1);
+    % vcNewGraphWin; imshow(tile);
+    
+    tileImages(:,:,ii) = tile;
+    
+    % Write out the small image time
+    cd(subImageDir)
+    imwrite(tile,fname);
+    
+    cd(originalTilesDir);
+end
 
 
 
